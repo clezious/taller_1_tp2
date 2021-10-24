af77e56dd5ed01ebb305358a8915e046b2720cc1
clezio <manuel.reberendo@cardinalsisa.com>
2021-10-24 03:11:14 -0300

    primer versión funcional

--- main.cpp
+++ main.cpp
@@ -25,18 +25,55 @@ void processTask(DatasetTaskResults& results,
     if (operation == "min"){
         result = dataset.column_min(column);
     }
-    results.update(task_id, result, (row_end - row_start),operation);
+    results.update(task_id, result, (row_end - row_start),operation);    
+}
+
+// Genera un vector cuyas entradas son las sub-string presentes en la string dada,
+// separadas por la string delimitadora.
+std::vector<std::string> split(std::string string, std::string delimiter){
+    std::vector<std::string> split;
+    int start_pos = 0;
+    int end_pos = string.find(delimiter);
+    while (end_pos != -1) {
+        split.push_back(string.substr(start_pos, end_pos - start_pos));
+        start_pos = end_pos + delimiter.size();
+        end_pos = string.find(delimiter, start_pos);
+    }
+    split.push_back(string.substr(start_pos, end_pos - start_pos));
+    return split;
 }
 
 int main(int argc, char const *argv[]){
     std::cout << argv[1] << '\n';    
     Dataset dataset(argv[1],strtol(argv[2],NULL,10),0,15);   
-    dataset.print();         
+    // dataset.print();         
     DatasetTaskResults results;
-    processTask(results,argv[1],0,0,3,strtol(argv[2],NULL,10),1,"sum");
-    processTask(results,argv[1],1,0,3,strtol(argv[2],NULL,10),1,"max");
-    processTask(results,argv[1],2,0,3,strtol(argv[2],NULL,10),1,"min");
-    processTask(results,argv[1],3,0,3,strtol(argv[2],NULL,10),1,"mean");
+    long task_id = 0;
+    for (std::string line; std::getline(std::cin, line);) {
+        std::vector<std::string> command = split(line," ");
+        unsigned long start_row = std::stol(command[0]);
+        unsigned long end_row = std::stol(command[1]);
+        unsigned long partition_size = std::stol(command[2]);
+        unsigned long column = std::stol(command[3]);
+        for (unsigned long partition_start = start_row;
+             partition_start < end_row;
+             partition_start += partition_size){
+            unsigned long partition_end = partition_start + partition_size;
+            if(partition_end > end_row){
+                partition_end = end_row;
+            }             
+            processTask(results,
+                        argv[1],
+                        task_id,
+                        partition_start,
+                        partition_end,
+                        strtol(argv[2],NULL,10),
+                        column,
+                        command[4]);            
+        }
+        
+        task_id ++;
+    }
     results.print();
     return 0;
 }
